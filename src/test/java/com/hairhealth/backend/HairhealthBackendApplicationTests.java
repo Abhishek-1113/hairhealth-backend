b9bc8cf51f403bb99324ed6e1cd309bf2b706562
Abhishek Chavan <abhi13n97@gmail.com>
2026-05-05 16:31:59 +0100

    Initial Spring Boot project

---
+++ src/test/java/com/hairhealth/backend/HairhealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hairhealth.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairhealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
